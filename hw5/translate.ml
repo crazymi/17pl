c53a5f8e52895c897464f24836caefa9068e3d44
crazymi <crazymi@snu.ac.kr>
2017-11-14 17:55:37 +0900

    implemented arithmetic/logical op, if, seq

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -11,11 +11,36 @@ module Translator = struct
   (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
+    | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
+    | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
+    | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
+    | K.VAR id -> [Sm5.PUSH (Sm5.Id id)]
+
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> (trans e1)@(trans e2)@([Sm5.SUB])
+    | K.MUL (e1, e2) -> (trans e1)@(trans e2)@([Sm5.MUL])
+    | K.DIV (e1, e2) -> (trans e1)@(trans e2)@([Sm5.DIV])
+
+    | K.EQUAL (e1, e2) -> (trans e1)@(trans e2)@([Sm5.EQ])
+    | K.LESS (e1, e2) -> (trans e1)@(trans e2)@([Sm5.LESS])
+    | K.NOT e -> (trans e)@([Sm5.NOT])
+
+    | K.ASSIGN (id, e) -> (trans e)@([Sm5.BIND id])
+    | K.SEQ (e1, e2) -> (trans e1)@(trans e2)
+    | K.IF (e1, e2, e3) -> (trans e1)@[Sm5.JTR (trans e2, trans e3)]
+    | K.WHILE (e1, e2) -> failwith "while unimplemented"
+    | K.FOR (id, e1, e2, e3) -> failwith "for unimplemented"
+
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.LETF (id, param, body, e) -> failwith "letf unimplemented"
+
+    | K.CALLV (id, e) -> failwith "callv unimplemented"
+    | K.CALLR (fid, aid) -> failwith "callr unimplemented"
+
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.WRITE e -> (trans e)@([Sm5.PUT])
     | _ -> failwith "Unimplemented"
 
 end
