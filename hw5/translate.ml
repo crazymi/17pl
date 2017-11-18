b97efd3a94828ac4c3dec5f85f075d05081e8133
crazymi <crazymi@snu.ac.kr>
2017-11-18 11:12:57 +0900

    fixed VAR, ASSIGN : testcase 1,6 work

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -14,7 +14,7 @@ module Translator = struct
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
     | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
     | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
-    | K.VAR id -> [Sm5.PUSH (Sm5.Id id)]
+    | K.VAR id -> [Sm5.PUSH (Sm5.Id id); Sm5.LOAD]
 
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
     | K.SUB (e1, e2) -> (trans e1)@(trans e2)@([Sm5.SUB])
@@ -25,7 +25,7 @@ module Translator = struct
     | K.LESS (e1, e2) -> (trans e1)@(trans e2)@([Sm5.LESS])
     | K.NOT e -> (trans e)@([Sm5.NOT])
 
-    | K.ASSIGN (id, e) -> (trans e)@([Sm5.BIND id])
+    | K.ASSIGN (id, e) -> (trans e)@([Sm5.PUSH (Sm5.Id id); Sm5.STORE])
     | K.SEQ (e1, e2) -> (trans e1)@(trans e2)
     | K.IF (e1, e2, e3) -> (trans e1)@[Sm5.JTR (trans e2, trans e3)]
     | K.WHILE (e1, e2) -> failwith "while unimplemented"
