908c59aec3848a316fe34796a428624ab17514a5
crazymi <crazymi@snu.ac.kr>
2017-11-27 20:29:22 +0900

    resolve @cont duplication, add continuation

--- hw6/ex1/rozetta.ml
+++ hw6/ex1/rozetta.ml
@@ -18,39 +18,44 @@ let rec trans_obj : Sm5.obj -> Sonata.obj = function
   | Sm5.Val v -> Sonata.Val (trans_v v)
   | Sm5.Id id -> Sonata.Id id
   | Sm5.Fn (arg, command) ->
+      let addition = (load "@cont") @ [Sonata.PUSH (Sonata.Val (Sonata.B true)); Sonata.MALLOC; Sonata.CALL] in
       Sonata.Fn (arg, (
         (save "@cont") @ (* note that, Before calling, @cont is at top of stack *)
-        (trans' command) @
+        (trans' command addition)
+      ))
+        (*@
         (load "@cont") @
         [Sonata.PUSH (Sonata.Val (Sonata.B true)); Sonata.MALLOC; Sonata.CALL]))
+      *)
 
-and trans': Sm5.command -> Sonata.command = function
-  | Sm5.PUSH obj :: cmds -> Sonata.PUSH (trans_obj obj) :: (trans' cmds)
-  | Sm5.POP :: cmds -> Sonata.POP :: (trans' cmds)
-  | Sm5.STORE :: cmds -> Sonata.STORE :: (trans' cmds)
-  | Sm5.LOAD :: cmds -> Sonata.LOAD :: (trans' cmds)
-  | Sm5.JTR (c1, c2) :: cmds -> [Sonata.JTR (trans' (c1@cmds), trans' (c2@cmds))]
-  | Sm5.MALLOC :: cmds -> Sonata.MALLOC :: (trans' cmds)
-  | Sm5.BOX z :: cmds -> Sonata.BOX z :: (trans' cmds)
-  | Sm5.UNBOX id :: cmds -> Sonata.UNBOX id :: (trans' cmds)
-  | Sm5.BIND id :: cmds -> Sonata.BIND id :: (trans' cmds)
-  | Sm5.UNBIND :: cmds -> Sonata.UNBIND :: (trans' cmds)
-  | Sm5.GET ::cmds -> Sonata.GET :: (trans' cmds)
-  | Sm5.PUT ::cmds -> Sonata.PUT :: (trans' cmds)
+and trans' : Sm5.command -> Sonata.command -> Sonata.command = fun cmdlist add ->
+  match cmdlist with
+  | Sm5.PUSH obj :: cmds -> Sonata.PUSH (trans_obj obj) :: (trans' cmds add)
+  | Sm5.POP :: cmds -> Sonata.POP :: (trans' cmds add)
+  | Sm5.STORE :: cmds -> Sonata.STORE :: (trans' cmds add)
+  | Sm5.LOAD :: cmds -> Sonata.LOAD :: (trans' cmds add)
+  | Sm5.JTR (c1, c2) :: cmds -> [Sonata.JTR (trans' (c1@cmds) add, trans' (c2@cmds) add)] @ []
+  | Sm5.MALLOC :: cmds -> Sonata.MALLOC :: (trans' cmds add)
+  | Sm5.BOX z :: cmds -> Sonata.BOX z :: (trans' cmds add)
+  | Sm5.UNBOX id :: cmds -> Sonata.UNBOX id :: (trans' cmds add)
+  | Sm5.BIND id :: cmds -> Sonata.BIND id :: (trans' cmds add)
+  | Sm5.UNBIND :: cmds -> Sonata.UNBIND :: (trans' cmds add)
+  | Sm5.GET ::cmds -> Sonata.GET :: (trans' cmds add)
+  | Sm5.PUT ::cmds -> Sonata.PUT :: (trans' cmds add)
   | Sm5.CALL :: cmds ->
       (save "@l") @ (save "@v") @ (save "@fn") @
-      [Sonata.PUSH (Sonata.Fn ("@cont", (trans' cmds)))] @
+      [Sonata.PUSH (Sonata.Fn ("@cont2", (trans' cmds add)))] @
       (load "@fn") @ (load "@v") @ (load "@l") @
       [Sonata.CALL]
-  | Sm5.ADD :: cmds -> Sonata.ADD :: (trans' cmds)
-  | Sm5.SUB :: cmds -> Sonata.SUB :: (trans' cmds)
-  | Sm5.MUL :: cmds -> Sonata.MUL :: (trans' cmds)
-  | Sm5.DIV :: cmds -> Sonata.DIV :: (trans' cmds)
-  | Sm5.EQ :: cmds -> Sonata.EQ :: (trans' cmds)
-  | Sm5.LESS :: cmds -> Sonata.LESS :: (trans' cmds)
-  | Sm5.NOT :: cmds -> Sonata.NOT :: (trans' cmds)
-  | [] -> []
+  | Sm5.ADD :: cmds -> Sonata.ADD :: (trans' cmds add)
+  | Sm5.SUB :: cmds -> Sonata.SUB :: (trans' cmds add)
+  | Sm5.MUL :: cmds -> Sonata.MUL :: (trans' cmds add)
+  | Sm5.DIV :: cmds -> Sonata.DIV :: (trans' cmds add)
+  | Sm5.EQ :: cmds-> Sonata.EQ :: (trans' cmds add)
+  | Sm5.LESS :: cmds -> Sonata.LESS :: (trans' cmds add)
+  | Sm5.NOT :: cmds -> Sonata.NOT :: (trans' cmds add)
+  | [] -> add
 
 (* TODO : complete this function *)
 let trans : Sm5.command -> Sonata.command = fun command ->
-  trans' command
+  trans' command []
